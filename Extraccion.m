33952b39b80f273402d1e9567290de91a2978557
J. Alberto Lopez <jalbertolopez10@outlook.com>
2016-09-16 19:26:34 -0500

    Recorrido de marca

--- Extraccion.m
+++ Extraccion.m
@@ -8,3 +8,10 @@ texto = fopen( strcat( rutaTxt, nombreTxt ) );
 caracteres8Bits = fread( texto, '*uint8' );
 longuitudTexto = length( caracteres8Bits );
 textoVector = [ ];
+
+for fila = 1 : m
+    for columna = 1 : n
+        pixel = imagen( fila, columna );
+        bitExtraido = bitget( pixel,1 );
+        caracter8Bits = strcat( caracter8Bits, ...
+										num2str( bitExtraido ) );
